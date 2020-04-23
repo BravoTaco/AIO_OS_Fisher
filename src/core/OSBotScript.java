49e194906849b3d1faa2b69c615ff03e0d5a1fd8
BravoTaco <snellcody9794@gmail.com>
2020-04-23 15:44:27 -0500

    Changed icon image

--- src/core/OSBotScript.java
+++ src/core/OSBotScript.java
@@ -18,7 +18,7 @@ import tasks.*;
 
 import java.awt.*;
 
-@ScriptManifest(info = "AIO OS Fisher.", version = 0.1, logo = "https://i.imgur.com/4aiCsAb.png",
+@ScriptManifest(info = "AIO OS Fisher.", version = 0.1, logo = "https://i.imgur.com/sSDEc20.jpg",
         author = "BravoTaco", name = "AIO OS Fisher")
 public class OSBotScript extends Script {
 
